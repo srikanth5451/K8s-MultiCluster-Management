0c556a55fc9596b973c8d6a976cc58280782e805
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2021-07-28 10:43:00 +0100

    docs: Cleanup old configurations

--- scripts/cluster-health-check.sh
+++ scripts/cluster-health-check.sh
@@ -25,3 +25,5 @@ jq -r '.items[] | .metadata.name + " " + .status.conditions[].status'
 # 2021-02-24 - ci: Configure multi-cluster tests
 
 # 2021-01-20 - chore: Refactor cluster manifests
+
+# 2021-07-28 - docs: Cleanup old configurations
